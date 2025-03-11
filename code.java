8803075944143b0f689661c14f3ed0d62f7668cd
null-ptr256 <agent.47xt@gmail.com>
2025-03-11 11:36:40 +0600

    Create code.java

---
+++ code.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class M {
+static Scanner s = new Scanner(System.in);
+static List<String> l = new ArrayList<>();
+public static void main(String[] a){
+  while(true){
+System.out.println("1: Add 2: Rm 3: Show 4: Ex");
+int c = s.nextInt();
+ if(c==1){
+ System.out.print("N: ");
+ s.nextLine();
+   l.add(s.nextLine());
+   }
+else if(c==2){
+System.out.print("I: ");
+int i = s.nextInt();
+   if(i>=0&&i<l.size())l.remove(i);
+ }
+ else if(c==3){
+for(int j=0;j<l.size();j++)
+ {System.out.println(j+": "+l.get(j));}
+}
+  else if(c==4)break;}}}
